27a9fed0377989ddcf82da72f0df473bc15c7a33
Dongseop Kim / 김동섭 <dongseop.kim@vuno.co>
2024-01-30 15:26:29 +0900

    add transforms-test(#8)
    random windowing이랑 transform-test 추가

--- univdt/transforms/builder.py
+++ univdt/transforms/builder.py
@@ -1,5 +1,5 @@
-from .pixel import (random_blur, random_brightness, random_contrast,
-                    random_gamma, random_noise)
+from .pixel import (random_blur, random_brightness, random_contrast, random_gamma, random_noise,
+                    random_windowing)
 from .resize import random_resize
 from .zoom import random_zoom
 
@@ -9,4 +9,5 @@ AVAILABLE_TRANSFORMS = {'random_blur': random_blur,
                         'random_gamma': random_gamma,
                         'random_noise': random_noise,
                         'random_resize': random_resize,
+                        'random_windowing': random_windowing,
                         'random_zoom': random_zoom, }
