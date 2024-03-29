d77eaca560ecca55c16edb19956514d31814dded
dongseopkim <dongseop.kim@vuno.co>
2024-03-29 20:22:04 +0900

    update scripts

--- scripts/pascal_voc.sh
+++ scripts/pascal_voc.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-
 # google drive link
 # https://drive.google.com/file/d/1_IodNv7NLbXKjE5MyB5WY8zGCtPyQ8Vp/view?usp=sharing
 file_id="1_IodNv7NLbXKjE5MyB5WY8zGCtPyQ8Vp"
-output_file="PASCAL-VOC.zip"
-
+output_file="voc.zip"
 gdown https://drive.google.com/uc?id=${file_id} -O ${output_file}
 
-# Unzip the file
 unzip ${output_file}
-
-# Remove the .zip file after extraction (optional, you can remove this line if you want to keep the .zip file)
 rm ${output_file}
