9b650bc09260cdd701c666ee26fe5d81b5143a6a
dongseopkim <dongseop.kim@vuno.co>
2024-10-04 13:59:09 +0900

    update test code

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -14,8 +14,7 @@ TEST_IMAGE1 = np.random.randint(0, 255, (768, 512, 3), dtype=np.uint8)
 TEST_IMAGE2 = np.random.randint(0, 255, (512, 768, 1), dtype=np.uint8)
 TEST_IMAGES = [TEST_IMAGE1, TEST_IMAGE2]
 
-_TRANSFORMS = {'random_windowing': {'magnitude': 0.5, 'p': 1.0},
-               'random_blur': {'magnitude': 0.5, 'p': 1.0},
+_TRANSFORMS = {'random_blur': {'magnitude': 0.5, 'p': 1.0},
                'random_gamma': {'magnitude': 0.5, 'p': 1.0},
                'random_brightness': {'magnitude': 0.5, 'p': 1.0},
                'random_clahe': {'magnitude': 0.5, 'p': 1.0},
